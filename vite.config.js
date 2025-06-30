35089321caaedbb6d49ff722ccccbdd36a2fe499
Siddhantt06 <crce.9966.ce@gmail.com>
2025-06-30 22:09:08 +0530

    fix: vite downgraded for Vercel compatibility

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
- base: '/suraj-cashew-website/',
-
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
